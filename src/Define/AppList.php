8095d22a567b011093af9014297a03aa354652da
xiaoming <xiaoming@qq.com>
2020-09-04 16:48:56 +0800

    add

---
+++ src/Define/AppList.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: email@gmail.com
+ * Date: 2020-09-04
+ * Time: 16:41
+ */
+
+namespace QI\Component\Define;
+
+class AppList
+{
+    const APP_NAME_CAOMEI_CHAT   = 'test';
+
+    const APP_LIST = [
+        self::APP_NAME_CAOMEI_CHAT
+    ];
+}
